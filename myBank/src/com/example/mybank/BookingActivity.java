54186a3083061fdc56dc7081b45f5cd7f40c2240
meikelmosby <meikelmosby@meikels-mbp>
2014-08-22 15:11:51 +0200

    #SettingScreen designed (Activity, xml )

--- myBank/src/com/example/mybank/BookingActivity.java
+++ myBank/src/com/example/mybank/BookingActivity.java
@@ -3,11 +3,11 @@ package com.example.mybank;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+
+
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
--- myBank/src/com/example/mybank/MainActivity.java
+++ myBank/src/com/example/mybank/MainActivity.java
@@ -2,23 +2,14 @@ package com.example.mybank;
 
 import java.util.Timer;
 import java.util.TimerTask;
-
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.os.Build;
 
 public class MainActivity extends Activity {
 
