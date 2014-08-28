b6255cb1415305a2b6148e0437290645738f0364
meikelmosby <meikelmosby@meikels-mbp>
2014-08-28 16:40:59 +0200

    #update

--- myBank/src/com/example/mybank/Settings_Banking_Activity.java
+++ myBank/src/com/example/mybank/Settings_Banking_Activity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.os.Build;
@@ -23,6 +24,8 @@ public class Settings_Banking_Activity extends Activity {
 	TextView AddButton_Limit;
 	TextView AddButton_Goal;
 	Spinner Spinner_Change_Currency;
+	Button Button_Default_Settings;
+	Button Button_Delete_History;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +67,12 @@ public class Settings_Banking_Activity extends Activity {
 		
 		AddButton_Limit = (TextView) findViewById(R.id.AddButton_Change_Goal);
 		AddButton_Limit.setText(R.string.AddButton_String_Plus);
+		
+		Button_Default_Settings = (Button) findViewById(R.id.Button_Settings_banking_default_settings);
+		Button_Default_Settings.setText(R.string.String_Button_Default_Settings);
+		
+		Button_Delete_History = (Button) findViewById(R.id.Button_Settings_banking_delete_history);
+		Button_Delete_History.setText(R.string.String_Button_Delete_History);
 		
 	}
 
